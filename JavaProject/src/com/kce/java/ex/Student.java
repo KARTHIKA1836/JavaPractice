4e1013d95854c6c3a0d0688c956c90d46af9b514
KARTHIKA1836 <karthikatn82@gmail.com>
2025-12-10 20:48:30 +0530

    initial commit

---
+++ JavaProject/src/com/kce/java/ex/Student.java
@@ -0,0 +1,15 @@
+package com.kce.java.ex;
+
+public class Student {
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=5;
+		int b=3;
+		int sum;
+		 System.out.println(sum=a+b);
+       
+	}
+
+}
